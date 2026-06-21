1310fdec8ead5b836d8055bc56aeb49377b4a993
eoyvillos037toxj <eoyvillos037toxj@users.noreply.github.com>
2026-06-20 22:08:23 -0700

    Add workflow auto-workflow.yml
    DiffTrain build for [c79a4a465bd6682f76ca7a7137ba10f93dd077ff](https://github.com/react/react/commit/c79a4a465bd6682f76ca7a7137ba10f93dd077ff)

--- compiled/facebook-www/React-profiling.modern.js
+++ compiled/facebook-www/React-profiling.modern.js
@@ -614,7 +614,7 @@ exports.useSyncExternalStore = function (
 exports.useTransition = function () {
   return ReactSharedInternals.H.useTransition();
 };
-exports.version = "19.3.0-www-modern-900ae094-20260605";
+exports.version = "19.3.0-www-modern-c79a4a46-20260621";
 "undefined" !== typeof __REACT_DEVTOOLS_GLOBAL_HOOK__ &&
   "function" ===
     typeof __REACT_DEVTOOLS_GLOBAL_HOOK__.registerInternalModuleStop &&
